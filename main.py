e25228d418223de927098ebf24d382c80d712967
Maximrudnicki <71455612+Maximrudnicki@users.noreply.github.com>
2022-09-04 21:30:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     cartoon1 = Cartoon("Battle", 2022, 153, "Savchuk")
     cartoon2 = Cartoon("Victory", 2022, 53, "Savchuk")
 
-    # print(cartoon1.__str__())
-
     cartoonList.add(cartoon1)
     cartoonList.add(cartoon2)
 
